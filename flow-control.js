8548f970b5f306b78278f7308c8744822933deb2
confident-processor-8379 <lancejcasey@gmail.com>
2019-03-26 02:06:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age > 12 && age < 20) {
   console.log("You are a teenager!")
+} 
+
+function teenager(age > 12 && age < 20) {
+  console.log("You are a teenager!")
 } else {
   console.log("You are not a teengaer")
-}
-
-function teenager(age) {
-
 }
 
 function ageChecker(age) {
